1edce2b1d4ee15bdf4bf9fe52bea3c84213d0e01
shank <shank@DESKTOP-5QGUCI3>
2019-08-13 17:53:15 +0530

    commit rep2

---
+++ hrb/src/hrb/Node.java
@@ -0,0 +1,14 @@
+package hrb;
+
+
+public class Node {
+
+int data;
+	Node Next;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hrb/src/hrb/Smpl2.java
@@ -0,0 +1,13 @@
+package hrb;
+
+public class Smpl2 {
+	Node head;
+	
+	public void insert(int data)
+	{
+		Node node=new Node();
+		node.data=data;
+		
+	}
+
+}
